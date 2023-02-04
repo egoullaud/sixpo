7617597d6af109482f9af3360e77ec53bab185e5
elizabeth.goullaud <egoullaud@gmail.com>
2023-02-04 13:09:16 -0800

    moved photos to assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import NavBar from '../components/NavBar'
+import heroImg from '../assets/disabled-line.jpg'
 
 function Home() {
   return (
     <div>
       <NavBar/>
+      <div className=''>
+    <img src={heroImg} alt="/" />
+      </div>
       <Footer/>
     </div>
   )
