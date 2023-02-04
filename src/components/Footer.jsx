c1c15194cf49a08c1b0807226f880be23b5a0f8d
elizabeth.goullaud <egoullaud@gmail.com>
2023-02-04 12:53:21 -0800

    built desktop footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/sixpo-logo.png'
 
 function Footer() {
+    // !add responsive functionality
   return (
-    <div>
-      
-    </div>
+    <div className='flex justify-between items-center bg-black text-white h-[13vh] px-2'>
+      <div className='w-[15%] items-center'>
+           <Link to='/'><img src={logo} alt="/" /> </Link> 
+      </div>
+        <div className='w-[85%]'>
+            <p className='text-center p-4 text-sm'> Members of SIXPO respectfully acknowledge that we live, learn and gather on the unceded, traditional and ancestral territories of the xʷməθkʷəy̓əm (Musqueam), Sḵwx̱wú7mesh Úxwumixw (Squamish), səl̓ilw̓ətaʔɬ (Tsleil-Waututh) the Katzie, Kwantlen, Kwikwetlem, Qayqayt and Tla'amin Nations.
+            </p>
+            <p className='text-center text-xs'>Copyright Lee Goullaud 2023</p>
+        </div>
+      </div>
+    
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Footer from '../components/Footer'
 import NavBar from '../components/NavBar'
 
 function Home() {
   return (
     <div>
       <NavBar/>
+      <Footer/>
     </div>
   )
 }
