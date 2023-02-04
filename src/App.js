21d9e8159f2b372b9f8b555ae39cc3e5c274cecd
elizabeth.goullaud <egoullaud@gmail.com>
2023-02-03 16:30:14 -0800

    set up file structure and connected pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import BlogPage from './pages/BlogPage';
+import Events from './pages/Events';
+import Home from './pages/Home';
+import Resources from './pages/Resources';
 
 function App() {
   return (
     <div className="App">
-     
+     <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/events" element={<Events/>}/>
+      <Route path="/resources" element={<Resources/>}/>
+      <Route path="/blog" element={<Blog/>}/>
+      <Route path="/blog/:slug" element={<BlogPage.jsx/>}/>
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
