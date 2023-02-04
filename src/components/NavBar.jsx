ece1c1c06d476d9dd102fe3e05693e7d2aea00f1
elizabeth.goullaud <egoullaud@gmail.com>
2023-02-04 12:26:35 -0800

    built out desktop navbar connected to home

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import logo from '../assets/sixpo-logo.png'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
+// !add drawer function
   return (
     <div>
-      
+        <div className='flex h-[13vh] px-4 bg-black text-white'>
+            <ul className='flex justify-around items-center uppercase'>
+                <li><Link to='/about'>About Us</Link></li> 
+                <li><Link to='/events'>Events</Link></li>
+                <li className='w-[15%]'>
+                    <Link to='/'> 
+                        <img className='w-[100%] justify-center items-center' src={logo} alt="/" />
+                    </Link>
+                   
+                </li>
+                <li><Link to='/blog'>Our Blog</Link></li>
+                <li><Link to='/resources'>Resources</Link></li>
+            </ul>
+        </div>
+  
     </div>
   )
 }
