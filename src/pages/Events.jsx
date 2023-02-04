dc1a2de4c75d9af0140d7fcab8715c04628efaa2
elizabeth.goullaud <egoullaud@gmail.com>
2023-02-04 12:54:23 -0800

    footer added to pages

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Footer from '../components/Footer'
 import NavBar from '../components/NavBar'
 
 function Events() {
   return (
     <div>
       <NavBar/>
+      <Footer/>
     </div>
   )
 }
