1983856643dc5511bd46011d2181ba3a9339910c
elizabeth.goullaud <egoullaud@gmail.com>
2023-02-21 18:31:24 -0800

    instaled graphql

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -7,12 +7,14 @@ function Resources() {
     <div className=''>
       <NavBar/>
       <section>
-        <div className='relative z-0 mt-[10rem]'>
-          <img className='w-[80%] mx-auto' src={heartBg} alt="" />
-          <div className='absolute md:top-[30%]'>
-              <h1>Resources</h1>
-          </div>
+     
+        <div className='relative z-0'>
+          <img className='w-[80%] mx-auto mt-[5rem]' src={heartBg} alt="" />
+          
         </div>
+        <div className='absolute md:top-[15%] w-[100%] mt-5'>
+              <h1 className='md:text-5xl font-bold w-[100%] text-center'>Resources</h1>
+          </div>
       </section>
       <Footer/>
     </div>
