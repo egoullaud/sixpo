95d79356bc874fd0bf41dd0d9bb1775b19a5406d
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-08 14:34:47 -0800

    added styling for queries

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Footer from '../components/Footer'
 import NavBar from '../components/NavBar'
+import BlogCard from '../components/BlogCard'
 
 function Blog() {
   return (
     <div>
       <NavBar/>
+      <div className='flex h-[60vh]'>
+        <div className='w-[20%] bg-gray-200'>
+          <h1 className='font-bold text-center text-xl my-4'>Categories</h1>
+          {/* add category query */}
+        </div>
+        <div className='w-[80%] bg-gray-300 flex flex-col justify-start items-center'>
+          <h1 className='font-bold text-center text-3xl my-4'>Articles</h1>
+          {/* <BlogCard/> */}
+          </div>
+      </div>
+      
       <Footer/>
     </div>
   )
