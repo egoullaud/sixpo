c5b15a56084d85f6c4516c8aef5829678b08fd77
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-09 14:24:28 -0800

    added about the author to post

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -62,9 +62,11 @@ function BlogPage() {
           <h1 className='font-bold text-center text-xl my-4'>Categories</h1>
           {/* add category query */}
         </div>
+        <div className='w-[80%]  flex flex-col justify-start items-center'>
+          <h1 className='font-bold text-center text-3xl my-4'>Articles</h1>
       <div className='flex items-center justify-center mt-[5rem]'>
           {posts.map((post) =>(
-              <div className='bg-white rounded shadow-md flex flex-col items-center justify-center w-[70%] mb-[5rem] 'key={post.id}>
+              <div className='bg-white rounded shadow-md flex flex-col items-center justify-center w-[90%] mb-[5rem] 'key={post.id}>
                   <img className='w-[95%]  object-cover rounded m-4'  src={post.image.url} alt="picture here" />
                   <h3 className=' w-[90%] text-center font-bold mx-4 md:text-xl lg:text-2xl mt-6' >{post.title}</h3>
                   <h4 className='text-base mt-3' >
@@ -73,12 +75,32 @@ function BlogPage() {
                   {return (`${author.name}`)})}  | {moment(post.datePublished).format('MMM DD, YYYY')} </h4>
                   <p className='text-base m-4 p-4 text-left ' dangerouslySetInnerHTML={{__html: post.content.html}}></p>
               
+              {/* about the author */}
+             
+              <div>
+              {post.authors?.map((author) => 
+                  (
+                   <div className=' bg-slate-100 p-5 rounded-md mx-[2rem] mb-[2rem]'>
+                    <h1 className='font-bold text-3xl'>About the Author</h1>
+                    <div className='flex flex-row  items-center justify-center '>
+                      <div className='flex flex-col items-center'>
+                      <img className='rounded-full w-[100%]' src={author.image.url}></img>
+                      <h1 className='font-normal text-2xl my-4'>{author.name}</h1>
+                      </div>
+                      
+                      <p className='p-[3rem] text-justify' dangerouslySetInnerHTML={{__html:author.content.html}}></p>
+
+                    </div>
+                    </div>
+                  ))}   
+              </div>
 
           </div>
 
         
          
         ))} 
+        </div>
         </div>
       </div>
       <Footer/>
