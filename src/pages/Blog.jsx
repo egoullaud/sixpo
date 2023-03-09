6460ffc968cee070d67338dcfd79dd6c91a2f928
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-08 17:38:38 -0800

    hook up api to display blog cards

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -7,14 +7,14 @@ function Blog() {
   return (
     <div>
       <NavBar/>
-      <div className='flex h-[60vh]'>
-        <div className='w-[20%] bg-gray-200'>
+      <div className='flex bg-gradient-to-t from-[#bcf0ff]'>
+        <div className='w-[20%]'>
           <h1 className='font-bold text-center text-xl my-4'>Categories</h1>
           {/* add category query */}
         </div>
-        <div className='w-[80%] bg-gray-300 flex flex-col justify-start items-center'>
+        <div className='w-[80%]  flex flex-col justify-start items-center'>
           <h1 className='font-bold text-center text-3xl my-4'>Articles</h1>
-          {/* <BlogCard/> */}
+          <BlogCard/>
           </div>
       </div>
       
