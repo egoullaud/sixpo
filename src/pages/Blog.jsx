75aa12d8cf0b5f4dcff369291a38749f9f1592a8
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-09 14:27:59 -0800

    hook up blog category component

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Footer from '../components/Footer'
 import NavBar from '../components/NavBar'
 import BlogCard from '../components/BlogCard'
+import BlogCategory from '../components/BlogCategory'
 
 function Blog() {
   return (
@@ -10,7 +11,7 @@ function Blog() {
       <div className='flex bg-gradient-to-t from-[#bcf0ff]  '>
         <div className='w-[20%]'>
           <h1 className='font-bold text-center text-xl my-4'>Categories</h1>
-          {/* add category query */}
+          <BlogCategory/>
         </div>
         <div className='w-[80%]  flex flex-col justify-start items-center'>
           <h1 className='font-bold text-center text-3xl my-4'>Articles</h1>
