f63c871afe08612a6650ecf25070d0546874df4a
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-09 14:51:49 -0800

    simplified category comp

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -2,7 +2,9 @@ import React, {useState, useEffect} from 'react'
 import Footer from '../components/Footer'
 import NavBar from '../components/NavBar'
 import moment from 'moment'
-import { Link } from 'react-router-dom';
+import BlogCard from '../components/BlogCard'
+import BlogCategory from '../components/BlogCategory'
+
 
 const POSTS_QUERY = `
 
@@ -58,10 +60,10 @@ function BlogPage() {
       <div>
         <NavBar/>
         <div className='flex bg-gradient-to-t from-[#bcf0ff]  '>
-        <div className='w-[20%]'>
-          <h1 className='font-bold text-center text-xl my-4'>Categories</h1>
-          {/* add category query */}
-        </div>
+       
+        
+          <BlogCategory/>
+        
         <div className='w-[80%]  flex flex-col justify-start items-center'>
           <h1 className='font-bold text-center text-3xl my-4'>Articles</h1>
       <div className='flex items-center justify-center mt-[5rem]'>
