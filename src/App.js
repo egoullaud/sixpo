fb2d60106add4cf5a26430596332cfcbe16195cd
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-09 14:06:58 -0800

    link blog posts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import About from './pages/About';
 import Blog from './pages/Blog';
+import BlogPage from './pages/BlogPage';
 
 import Events from './pages/Events';
 import Home from './pages/Home';
@@ -16,6 +17,7 @@ function App() {
       <Route path="/events" element={<Events/>}/>
       <Route path="/resources" element={<Resources/>}/>
       <Route path="/blog" element={<Blog/>}/>
+      <Route path="/blog/:slug" element={<BlogPage/>}/>
      
      </Routes>
      </BrowserRouter>
