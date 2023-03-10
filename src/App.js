c0ffe1d94b1a3bcbedfbd5c10b11e58b0ab75177
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-09 16:09:06 -0800

    moved nav, footer, browser router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
 import About from './pages/About';
 import Blog from './pages/Blog';
 import BlogPage from './pages/BlogPage';
-
 import Events from './pages/Events';
 import Home from './pages/Home';
 import Resources from './pages/Resources';
@@ -10,17 +11,16 @@ import Resources from './pages/Resources';
 function App() {
   return (
     <div className="App">
-     <BrowserRouter>
-     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/events" element={<Events/>}/>
-      <Route path="/resources" element={<Resources/>}/>
-      <Route path="/blog" element={<Blog/>}/>
-      <Route path="/blog/:slug" element={<BlogPage/>}/>
-     
-     </Routes>
-     </BrowserRouter>
+      <NavBar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/events" element={<Events/>}/>
+          <Route path="/resources" element={<Resources/>}/>
+          <Route path="/blog" element={<Blog/>}/>
+          <Route path="/blog/:slug" element={<BlogPage/>}/>
+      </Routes>
+     <Footer/>
     </div>
   );
 }
