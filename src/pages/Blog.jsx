e06735c09523aedd0e6e3e01f7f83005d0ff852b
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-09 19:54:06 -0800

    fixed category div formatting

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -4,11 +4,10 @@ import BlogCategory from '../components/BlogCategory'
 
 function Blog() {
   return (
-    <div className='flex'>
+    <div className='flex bg-gradient-to-t from-[#bcf0ff] '>
 
-      <div className='flex w-[20%] bg-gradient-to-t from-[#bcf0ff]  '>
           <BlogCategory/>
-        </div>
+       
         <div className='w-[80%]  flex flex-col justify-start items-center'>
           <h1 className='font-bold text-center text-5xl mt-[4rem]'>Articles</h1>
           <BlogCard/>
