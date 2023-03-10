973663a671f91c93971dbb11163a7a3d84ca0c7a
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-09 20:25:47 -0800

    playing with bg images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: [ "./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'heartBg' : "url('../public/heart.png')",
+        'blueTri' : "url('../public/bluetri.png')"
+      }
+    },
   },
   plugins: [],
 }
