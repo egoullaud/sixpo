d40093651ddcd4462d11ae477acee9338cb45d74
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-10 16:46:10 -0800

    added alt text for hero photo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,9 +12,9 @@ function Home() {
   return (
     <div >
      
-      <section id='hero'>
+      <section id='home-page-hero'>
         <div className='relative z-0'>
-          <img src={heroImg} alt="/" />
+          <img src={heroImg} alt="Six disabled people of color smile and pose in front of a concrete wall. Five people stand in the back, with the Black woman in the center holding up a chalkboard sign reading `disabled and here.` A South Asian person in a wheelchair sits in front." />
         <div className='absolute inset-0 bg-black bg-opacity-40 z-10 h-[6vh]'>
           <h1 className='h-[6vh] flex items-center justify-center text-white text-center text-4xl'>
            Sexuality | Inclusion | Exploration</h1>
