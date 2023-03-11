be2f57621d01173fbe4dbedd26f4f7f18f6a1e15
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-11 13:49:44 -0800

    updated responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,18 +13,17 @@ function Footer() {
         <div className='
           flex
           w-[100%]
-          lg:w-[15%] 
-          md:w-[15%] 
+          md:w-[20%] 
           justify-center 
           items-center'
         >
-            <Link className='w-[50%] lg:w-[60%]' to='/'>
+            <Link className='w-[50%] md:w-[100%]' to='/'>
                 <img src={logo} alt="SIXpo logo: a bright pink heart with the word SIXPO in yellow across the front and the word Festival printed in outlined letters below." /> 
             </Link> 
         </div>
         <div className='
         w-[100%]
-        lg:w-[85%]'
+        md:w-[85%]'
         >
           <p className='text-center p-4 mx-4 text-sm'> Members of SIXPO respectfully acknowledge that we live, learn and gather on the unceded, traditional and ancestral territories of the xʷməθkʷəy̓əm (Musqueam), Sḵwx̱wú7mesh Úxwumixw (Squamish), səl̓ilw̓ətaʔɬ (Tsleil-Waututh) the Katzie, Kwantlen, Kwikwetlem, Qayqayt and Tla'amin Nations.
            <br/> Non-Indigenous folks in Canada please take a look at 
