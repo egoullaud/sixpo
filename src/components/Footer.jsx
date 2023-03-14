908ad422a8f245107107189f6f2f7feb77b4a292
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-13 17:34:05 -0700

    lg styling social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,13 +12,16 @@ function Footer() {
         justify-between items-center bg-black text-white px-2'
     >
         <div className='
-          flex flex-col
+          flex 
+          flex-col
+          lg:flex-row
+          lg:justify-around
           w-[100%]
           md:w-[20%] 
           justify-center 
           items-center'
         >
-            <Link className='w-[50%] md:w-[100%]' to='/home'>
+            <Link className='w-[50%] md:w-[100%] lg:w-[50%]' to='/home'>
                 <img src={logo} alt="SIXpo logo: a bright pink heart with the word SIXPO in yellow across the front and the word Festival printed in outlined letters below." /> 
             </Link> 
             <ul className='flex mx-2 text-white text-2xl pt-2 bg-black'>
