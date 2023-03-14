4ebd3cca4dbe5295a3086923951d3a9de42464c1
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-13 17:38:38 -0700

    route '/'

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <div className="App">
       <NavBar/>
         <Routes>
+          <Route path='/' element={<Home/>}/>
           <Route path="/home" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
           <Route path="/events" element={<Events/>}/>
