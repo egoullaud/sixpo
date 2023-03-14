c6e9c99d44d5c6736d6bf0d7cee27d9f0cf1122f
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-13 17:12:02 -0700

    mobile finished

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,15 +9,15 @@ function NavBar() {
         setNav(!nav);
     }
   return (
-    <nav className='flex justify-between items-center h-[13vh] bg-black text-white'>
+    <nav className='flex justify-between items-center h-[13vh] bg-black text-white overflow-x-hidden'>
         {/* <nav className='
         h-[13vh] 
         md:flex  md:h-[13vh] md:px-4 
         lg:h-[15vh] 
          bg-black text-white font-bold'> */}
             
-            <h1 className='h-[13vh] w-[25%] flex  md:hidden '>
-                <Link to='/'> 
+            <h1 className='h-[13vh] w-[25%] flex  md:hidden z-10'>
+                <Link to='/home'> 
                     <img className='w-[100%] m-2 justify-center items-center ' src={logo} alt="SIXpo logo: a bright pink heart with the word SIXPO in yellow across the front and the word Festival printed in outlined letters below." />
                 </Link>
             </h1>
@@ -28,7 +28,7 @@ function NavBar() {
                 <li><Link className='lg:mx-[4rem] md:mx-[3rem] hover:transition-all hover:duration-500 hover:underline hover:text-[#ffef91]' to='/about'>About Us</Link></li> 
                 <li><Link className='lg:mx-[4rem md:mx-[3rem] hover:transition-all hover:duration-500 hover:underline hover:text-[#ffef91]' to='/events'>Events</Link></li>
                 <li className='lg:w-[11%] md:w-[15%]'>
-                    <Link to='/'> 
+                    <Link to='/home'> 
                         <img className='w-[100%] justify-center items-center' src={logo} alt="SIXpo logo: a bright pink heart with the word SIXPO in yellow across the front and the word Festival printed in outlined letters below." />
                     </Link>
                    
@@ -38,12 +38,12 @@ function NavBar() {
             </ul>
         {/* </nav> */}
         {/* mobile navigation */}
-        <div onClick={handleNav} className='block md:hidden mx-4 mb-2 z-10'>
+        <div onClick={handleNav} className='block md:hidden mx-4 mb-2 z-10 ease-in-out duration-500'>
                 {!nav ? <AiOutlineMenu size={25}/> : <AiOutlineClose size={25}/> }
         </div>
-        <div className={!nav ? 'fixed left-[-100%] h-full' : 'z-10 fixed left-0 top-0 bg-black h-full w-[60%] text-white border-r-2 border-gray-200 ease-in-out transition-all duration-500'}>
+        <div className={!nav ? 'fixed left-[-100%] h-[100%]' : 'z-10 fixed left-0 top-0 h-[100%] bg-black w-[60%] text-white border-r-2 border-gray-200 ease-in-out  duration-500'}>
         <h1 className='h-[13vh] w-[40%] flex  md:hidden '>
-                <Link to='/'> 
+                <Link to='/home'> 
                     <img className='w-[100%] m-2 justify-center items-center ' src={logo} alt="SIXpo logo: a bright pink heart with the word SIXPO in yellow across the front and the word Festival printed in outlined letters below." />
                 </Link>
             </h1>
