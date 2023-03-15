502a9a659f98c573818f53c07d490418c30153c8
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-15 16:25:16 -0700

    added space to top/bottom and h1

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -7,10 +7,10 @@ import ResourceCard from '../components/resources/ResourceCard'
 
 function Resources() {
   return (
-    <div className='flex flex-col  bg-black bg-opacity-70 ' >  
-     <h1 className='font-bold w-[100%] text-white text-center my-5 text-3xl
+    <div className='flex flex-col  bg-black bg-opacity-70 pb-[10rem]  ' >  
+     <h1 className='font-bold w-[100%] text-white text-center my-[5rem] text-3xl
      md:text-5xl'>Resources</h1>
-      <section className='flex  '>
+      <section className='flex '>
          <ResourceCard/>
       </section>
     </div>
