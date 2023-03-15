881f028a8ed2033954ae67c9ebc2aec03d078f80
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-15 16:02:26 -0700

    styled lg screens

--- src/components/resources/ResourceCard.jsx
+++ src/components/resources/ResourceCard.jsx
@@ -52,16 +52,18 @@ function ResourceCard() {
                       <img  className='rounded 
                       md:w-[35%] max-h-56 m-2' src={resource.image.url} alt={resource.image.altText} />
                  
-                    <div className='flex flex-col items-center text-center  mb-4 w-[90%]
-                                  md:text-left md:mr-4 md:w-[60%]
+                    <div className='flex flex-col items-center text-center mb-4 w-[90%]
+                                  md:text-left md:mr-4 md:w-[60%] md:items-start
                                   '>
                       <h3 className=' font-bold mx-4 mt-2 text-2xl
+                                 md:text-left
                                 lg:mt-6 
                                 ' >{resource.title}</h3>
                      <a href={resource.url} target="_blank" className=''> <h4 className=' text-blue-600  font-semibold mx-4 mt-2 
-                                md:text-lg
+                                md:text-lg  md:text-left
                                 '> Visit Website</h4></a>
-                      <p className='text-base mx-4 my-2 text-center' dangerouslySetInnerHTML={{__html:resource.content.html}}  ></p>
+                      <p className='text-base mx-4 my-2 text-center
+                       md:text-left' dangerouslySetInnerHTML={{__html:resource.content.html}}  ></p>
                     </div>
                     
 
