508e1740881bac1d1cf428b922d21f6f5ca2578b
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-15 14:30:32 -0700

    standardized card style, made responsive

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -57,17 +57,25 @@ function BlogCard() {
 
 
         return(
-        <div className='flex items-center justify-center mt-[5rem]'>
+        <div className='flex flex-col items-center justify-center'>
             {posts.map((post) =>(
-                <card className='bg-white rounded shadow-md flex  items-center justify-center w-[70%] mb-[5rem] 'key={post.id}>
-                   <Link className='w-[100%]  object-cover rounded m-4' to={"/blog/"+post.slug}> <img className=''  src={post.image.url} alt={post.image.altText} /></Link>
-                    <div className='flex flex-col'>
-                      <h3 className=' w-[90%] text-left font-bold mx-4 md:text-xl lg:text-2xl mt-6' >{post.title}</h3>
+                <card className='bg-white rounded shadow-md flex flex-col items-center justify-center
+                             w-[90%] 
+                             md:w-[90%] lg:flex-row'
+                      key={post.id}>
+                   <Link className='w-[100%] object-cover lg:ml-4' to={"/blog/"+post.slug}> 
+                      <img  className='rounded' src={post.image.url} alt={post.image.altText} />
+                   </Link>
+                    <div className='flex flex-col text-center mr-4 mb-4
+                                  md:text-left'>
+                      <h3 className=' w-[90%]  font-bold mx-4 mt-2 
+                                lg:mt-6 md:text-xl
+                                lg:text-2xl' >{post.title}</h3>
                     <h4 className='font-semibold mt-3 mx-4' >
                         {/* map over authors array */}
                         {post.authors?.map((author) => 
                     {return (`${author.name}`)})}  | {moment(post.datePublished).format('MMM DD, YYYY')} </h4>
-                    <p className='text-base mx-4 my-4 text-left'  >{post.preview} <Link className='font-semibold mx-1' to={"/blog/"+post.slug}><button className='underline hover:font-bold transition-all duration-700'>
+                    <p className='text-base mx-4 my-2'  >{post.preview} <Link className='font-semibold mx-1' to={"/blog/"+post.slug}><button className='underline hover:font-bold transition-all duration-700'>
                         Read More</button></Link></p>
                     </div>
                     
