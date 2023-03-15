1db4e4b56ed0c76bc5877f180d05650086f5d38c
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-15 14:31:05 -0700

    responsive

--- src/pages/SelectedBlogCategory.jsx
+++ src/pages/SelectedBlogCategory.jsx
@@ -28,13 +28,15 @@ const SELECTED_BLOG_CATEGORY_QUERY =`
 
 function SelectedBlogCategory() {
   return (
-    <div className='flex bg-gradient-to-t from-[#bcf0ff] '>
-
-    
-        <BlogCategory/>
+    <div className='flex bg-black bg-opacity-70  '>
+        <div className='hidden bg-white  m-[1rem] rounded shadow-lg
+            md:w-[20%] md:flex md:justify-center'>
+            <BlogCategory/>
+            </div>
       
-      <div className='w-[80%]  flex flex-col justify-start items-center'>
-        <h1 className='font-bold text-center text-5xl mt-[4rem]'>Articles</h1>
+      <div className='w-[80%]  flex flex-col justify-start items-center mb-[5rem]'>
+          <h1 className='font-bold text-center my-[4rem] mx-[1rem] text-white text-4xl
+                        md:text-5xl'>Welcome to our blog!</h1>
         <SelectedBlogCatCard/>
         </div>
   </div>
