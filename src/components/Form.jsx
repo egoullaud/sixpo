0c67ba9cbf31ee280d89d174aa813be0da2ac527
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-17 14:24:34 -0700

    fixed text color for inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -16,7 +16,7 @@ function Form() {
               type="name" 
               id="name" 
               name="name"
-              class=" bg-gray-50 border border-gray-300 text-white text-sm rounded-lg 
+              class=" bg-gray-50 border border-gray-300 text-black text-sm rounded-lg 
               focus:ring-primary-500 focus:border-[#bc] block w-full p-2.5
               " 
               placeholder="John Doe" required/>
@@ -24,35 +24,36 @@ function Form() {
            <div>
               <label for="email" class="block mb-2 text-sm font-medium text-white" >Your email</label>
               <input type="email" id="email" name="email address"
-              class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+              class=" bg-gray-50 border border-gray-300 text-black text-sm rounded-lg 
               focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5
               " 
               placeholder="johndoe@youremail.com" required/>
           </div>
           <div>
               <label for="text" class="block mb-2 text-sm font-medium text-white">Your Phone Number</label>
-              <input type="text" id="phone" name="phone number" class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+              <input type="text" id="phone" name="phone number" 
+              class=" bg-gray-50 border border-gray-300 text-black text-sm rounded-lg 
               focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" 
               placeholder="(555) 555-5555" />
           </div>
           <label for="where" class="block mb-2 text-sm font-medium text-white">Where did you hear about us?</label>
               <input type="text" id="where" placeholder="Tell us!" name="where did you hear about us" 
-              class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+              class=" bg-gray-50 border border-gray-300 text-black text-sm rounded-lg 
               focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5
               " 
               />
           <div class="flex flex-col ">
           <label class="flex  items-start mb-2 text-sm font-medium text-white" name='interested-in'>I'm interested in:</label>
             <div class='flex items-center justify-center text-white'>
-                <input type="checkbox" id="volunteering" class="text-white" name='volunteering' value="yes" />
+                <input type="checkbox" id="volunteering" class="text-black" name='volunteering' value="yes" />
                 <label htmlFor="volunteering" class='mx-2 text-sm'>Volunteering</label>
             </div>
             <div class='flex items-center justify-center text-white'>
-                <input type="checkbox" id="sponsoring" class="text-white" name="sponsoring" value="yes" />
+                <input type="checkbox" id="sponsoring" class="text-black" name="sponsoring" value="yes" />
                 <label htmlFor="sponsoring" class='ml-2 mr-5 my-1 text-sm'>Sponsoring</label>
             </div>
             <div class='flex items-center justify-center text-white'>
-                <input type="checkbox" id="collaborating" class="text-white" name='collaborating' value="yes" />
+                <input type="checkbox" id="collaborating" class="text-black" name='collaborating' value="yes" />
                 <label htmlFor="collaborating" class='mx-2 text-sm'>Collaborating</label>
             </div>
         
@@ -61,7 +62,7 @@ function Form() {
           <div class="sm:col-span-2">
               <label for="message" class="block mb-2 text-sm font-medium text-white">Your message</label>
               <textarea id="message" rows="6" 
-              class="block p-2.5 w-full text-sm bg-gray-50 rounded-lg border border-gray-300 
+              class="block p-2.5 w-full text-sm text-black bg-gray-50 rounded-lg border border-gray-300 
               focus:ring-primary-500 focus:border-primary-500
                " 
               placeholder="Write your message here..."></textarea>
