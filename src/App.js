714fc57860aa8f63b368e15916ed368baf387f72
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-22 18:52:47 -0700

    formatted with prettier

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import {Route, Routes} from 'react-router-dom';
-import Footer from './components/Footer';
-import NavBar from './components/NavBar';
-import About from './pages/About';
-import Blog from './pages/Blog';
-import BlogPage from './pages/BlogPage';
-import Events from './pages/Events';
-import Home from './pages/Home';
-import Resources from './pages/Resources';
-import SelectedBlogCategory from './pages/SelectedBlogCategory';
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import BlogPage from "./pages/BlogPage";
+import Events from "./pages/Events";
+import Home from "./pages/Home";
+import Resources from "./pages/Resources";
+import SelectedBlogCategory from "./pages/SelectedBlogCategory";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path="/home" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/events" element={<Events/>}/>
-          <Route path="/resources" element={<Resources/>}/>
-          <Route path="/blog" element={<Blog/>}/>
-          <Route path="/blog/:slug" element={<BlogPage/>}/>
-          <Route path="/posts/:slug" element={<SelectedBlogCategory/>}/> 
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<BlogPage />} />
+        <Route path="/posts/:slug" element={<SelectedBlogCategory />} />
       </Routes>
-     <Footer/>
+      <Footer />
     </div>
   );
 }
