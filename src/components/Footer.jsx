187db2a0b2dec70ed4bff8223160da50db85ab60
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-22 18:53:13 -0700

    formatted with prettier

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../assets/sixpo-logo.png'
-import {AiFillInstagram, AiFillFacebook} from 'react-icons/ai'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/sixpo-logo.png";
+import { AiFillInstagram, AiFillFacebook } from "react-icons/ai";
 
 function Footer() {
   return (
-    <div className='
+    <div
+      className="
         flex
         flex-col
         md:flex-row 
-        justify-between items-center bg-black text-white px-2'
+        justify-between items-center bg-black text-white px-2"
     >
-        <div className='
+      <div
+        className="
           flex 
           flex-col
           lg:flex-row
@@ -19,34 +21,57 @@ function Footer() {
           w-[100%]
           md:w-[20%] 
           justify-center 
-          items-center'
-        >
-            <Link className='w-[50%] md:w-[100%] lg:w-[50%]' to='/'>
-                <img src={logo} alt="SIXpo logo: a bright pink heart with the word SIXPO in yellow across the front and the word Festival printed in outlined letters below." /> 
-            </Link> 
-            <ul className='flex mx-2 text-white text-2xl pt-2 bg-black'>
-               <a href="https://www.instagram.com/yvrsixpo/" target="_blank"> <li className='mr-2'><AiFillInstagram/></li></a>
-               <a href="https://www.facebook.com/yvrsixpo/" target="_blank">  <li><AiFillFacebook/></li></a>
-            </ul>
-        </div>
-        <div className='
-        w-[100%]
-        md:w-[85%]'
-        >
-          <p className='text-center p-4 mx-4 text-sm'> Members of SIXPO respectfully acknowledge that we live, learn and gather on the unceded, traditional and ancestral territories of the xʷməθkʷəy̓əm (Musqueam), Sḵwx̱wú7mesh Úxwumixw (Squamish), səl̓ilw̓ətaʔɬ (Tsleil-Waututh) the Katzie, Kwantlen, Kwikwetlem, Qayqayt and Tla'amin Nations.
-           <br/> Non-Indigenous folks in Canada please take a look at 
-           <a href='https://www.whose.land/en/' target="_blank"> 
-           <span className='font-semibold text-[#ffef91] hover:underline hover:transition-all hover:duration-500 hover:text-[#ffa2a2] '>Whose Land</span> 
-           </a> to learn more about the indigenous nation(s) on whose land you live!
-            </p>
-            <a href='https://egoullaud-portfolio.netlify.app/' target="_blank">
-              <p className='text-center pb-2 text-xs underline hover:transition-all hover:duration-500 hover:text-[#bcf0ff]'>
-                Website by Lee Goullaud 2023</p>
-            </a>
-        </div>
+          items-center"
+      >
+        <Link className="w-[50%] md:w-[100%] lg:w-[50%]" to="/">
+          <img
+            src={logo}
+            alt="SIXpo logo: a bright pink heart with the word SIXPO in yellow across the front and the word Festival printed in outlined letters below."
+          />
+        </Link>
+        <ul className="flex mx-2 text-white text-2xl pt-2 bg-black">
+          <a href="https://www.instagram.com/yvrsixpo/" target="_blank">
+            {" "}
+            <li className="mr-2">
+              <AiFillInstagram />
+            </li>
+          </a>
+          <a href="https://www.facebook.com/yvrsixpo/" target="_blank">
+            {" "}
+            <li>
+              <AiFillFacebook />
+            </li>
+          </a>
+        </ul>
       </div>
-    
-  )
+      <div
+        className="
+        w-[100%]
+        md:w-[85%]"
+      >
+        <p className="text-center p-4 mx-4 text-sm">
+          {" "}
+          Members of SIXPO respectfully acknowledge that we live, learn and
+          gather on the unceded, traditional and ancestral territories of the
+          xʷməθkʷəy̓əm (Musqueam), Sḵwx̱wú7mesh Úxwumixw (Squamish), səl̓ilw̓ətaʔɬ
+          (Tsleil-Waututh) the Katzie, Kwantlen, Kwikwetlem, Qayqayt and
+          Tla'amin Nations.
+          <br /> Non-Indigenous folks in Canada please take a look at
+          <a href="https://www.whose.land/en/" target="_blank">
+            <span className="font-semibold text-[#ffef91] hover:underline hover:transition-all hover:duration-500 hover:text-[#ffa2a2] ">
+              Whose Land
+            </span>
+          </a>{" "}
+          to learn more about the indigenous nation(s) on whose land you live!
+        </p>
+        <a href="https://egoullaud-portfolio.netlify.app/" target="_blank">
+          <p className="text-center pb-2 text-xs underline hover:transition-all hover:duration-500 hover:text-[#bcf0ff]">
+            Website by Lee Goullaud 2023
+          </p>
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,80 +1,149 @@
-import React, { useState } from 'react'
-import logo from '../assets/sixpo-logo.png'
-import { Link } from 'react-router-dom'
-import {AiOutlineMenu, AiOutlineClose, AiFillInstagram, AiFillFacebook} from 'react-icons/ai'
+import React, { useState } from "react";
+import logo from "../assets/sixpo-logo.png";
+import { Link } from "react-router-dom";
+import {
+  AiOutlineMenu,
+  AiOutlineClose,
+  AiFillInstagram,
+  AiFillFacebook,
+} from "react-icons/ai";
 
 function NavBar() {
-    const [nav, setNav] = useState(false);
-    const handleNav = () => {
-        setNav(!nav);
-    }
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
   return (
-  
-         
-    <nav className='flex justify-between items-center h-[13vh] lg:h-[17vh] xl:h-[17vh] bg-black text-white'>
-  
-            <h1 className='h-[13vh] w-[25%] flex  md:hidden z-10'>
-                <Link to='/'> 
-                    <img className='w-[100%] m-2 justify-center items-center ' src={logo} alt="SIXpo logo: a bright pink heart with the word SIXPO in yellow across the front and the word Festival printed in outlined letters below." />
-                </Link>
-            </h1>
-            
-            <ul className=' hidden 
+    <nav className="flex justify-between items-center h-[13vh] lg:h-[17vh] xl:h-[17vh] bg-black text-white">
+      <h1 className="h-[13vh] w-[25%] flex  md:hidden z-10">
+        <Link to="/">
+          <img
+            className="w-[100%] m-2 justify-center items-center "
+            src={logo}
+            alt="SIXpo logo: a bright pink heart with the word SIXPO in yellow across the front and the word Festival printed in outlined letters below."
+          />
+        </Link>
+      </h1>
+
+      <ul
+        className=" hidden 
             md:flex md:justify-center md:items-center uppercase w-[100%] pt-2  
             
-            '>
-                <li><Link className='
+            "
+      >
+        <li>
+          <Link
+            className="
                 xl:mx-[1rem]
                 lg:mx-[4rem] 
                 md:mx-[2rem] 
-                hover:transition-all hover:duration-500 hover:underline hover:text-[#ffef91]' to='/about'>About Us</Link></li> 
-                <li><Link className='xl:mx-[1rem]
+                hover:transition-all hover:duration-500 hover:underline hover:text-[#ffef91]"
+            to="/about"
+          >
+            About Us
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="xl:mx-[1rem]
                 lg:mx-[4rem] 
                 md:mx-[2rem] 
-                hover:transition-all hover:duration-500 hover:underline hover:text-[#ffef91]' to='/events'>Events</Link></li>
-                <li className='lg:w-[11%] md:w-[15%]'>
-                    <Link to='/home'> 
-                        <img className='w-[100%] justify-center items-center' src={logo} alt="SIXpo logo: a bright pink heart with the word SIXPO in yellow across the front and the word Festival printed in outlined letters below." />
-                    </Link>
-                   
-                </li>
-                <li><Link className='xl:mx-[1rem]
+                hover:transition-all hover:duration-500 hover:underline hover:text-[#ffef91]"
+            to="/events"
+          >
+            Events
+          </Link>
+        </li>
+        <li className="lg:w-[11%] md:w-[15%]">
+          <Link to="/home">
+            <img
+              className="w-[100%] justify-center items-center"
+              src={logo}
+              alt="SIXpo logo: a bright pink heart with the word SIXPO in yellow across the front and the word Festival printed in outlined letters below."
+            />
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="xl:mx-[1rem]
                 lg:mx-[4rem] 
                 md:mx-[2rem] 
-                hover:transition-all hover:duration-500 hover:underline hover:text-[#ffef91]' to='/blog'>Our Blog</Link></li>
-                <li><Link className='xl:mx-[1rem]
+                hover:transition-all hover:duration-500 hover:underline hover:text-[#ffef91]"
+            to="/blog"
+          >
+            Our Blog
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="xl:mx-[1rem]
                 lg:mx-[4rem] 
                 md:mx-[2rem] 
-                hover:transition-all hover:duration-500 hover:underline hover:text-[#ffef91]' to='/resources'>Resources</Link></li>
-            </ul>
-           
-            
-      
-        {/* mobile navigation */}
-        <div onClick={handleNav} className='block md:hidden mx-4 mb-2 z-20 ease-in-out duration-500'>
-                {!nav ? <AiOutlineMenu size={25}/> : <AiOutlineClose size={25}/> }
-        </div>
-        <div className={!nav ? 'absolute left-[-100%] h-[100%] ' : 'z-10 absolute left-0 top-0 h-[100%] bg-black w-[60%] text-white  ease-in-out  duration-500'}>
-        <h1 className='h-[13vh] w-[40%] flex  md:hidden '>
-                <Link to='/'> 
-                    <img className='w-[100%] m-2 justify-center items-center ' src={logo} alt="SIXpo logo: a bright pink heart with the word SIXPO in yellow across the front and the word Festival printed in outlined letters below." />
-                </Link>
-            </h1>
-        <ul className='flex flex-col items-start uppercase w-[100%] pt-2  md:hidden '>
-                <li className='my-1 p-2 w-[100%]  hover:ease-in-out hover:duration-500 hover:underline hover:text-[#ffef91]'><Link  to='/home'>Home</Link></li> 
-                <li className='my-1 p-2 w-[100%]  hover:ease-in-out hover:duration-500 hover:underline hover:text-[#ffef91]'><Link  to='/about'>About Us</Link></li> 
-                <li className='my-1 p-2 w-[100%]  hover:ease-in-out hover:duration-500 hover:underline hover:text-[#ffef91]'><Link  to='/events'>Events</Link></li>
-                <li className='my-1 p-2 w-[100%]  hover:ease-in-out hover:duration-500 hover:underline hover:text-[#ffef91]'><Link  to='/blog'>Our Blog</Link></li>
-                <li className='my-1 p-2 w-[100%]  hover:ease-in-out hover:duration-500 hover:underline hover:text-[#ffef91]'><Link  to='/resources'>Resources</Link></li>
-            </ul>
-            <ul className='flex mx-2 text-white text-2xl pt-2'>
-               <a href="https://www.instagram.com/yvrsixpo/" target="_blank"> <li className='mr-2'><AiFillInstagram/></li></a>
-               <a href="https://www.facebook.com/yvrsixpo/" target="_blank">  <li><AiFillFacebook/></li></a>
-            </ul>
-        </div>
+                hover:transition-all hover:duration-500 hover:underline hover:text-[#ffef91]"
+            to="/resources"
+          >
+            Resources
+          </Link>
+        </li>
+      </ul>
+
+      {/* mobile navigation */}
+      <div
+        onClick={handleNav}
+        className="block md:hidden mx-4 mb-2 z-20 ease-in-out duration-500"
+      >
+        {!nav ? <AiOutlineMenu size={25} /> : <AiOutlineClose size={25} />}
+      </div>
+      <div
+        className={
+          !nav
+            ? "absolute left-[-100%] h-[100%] "
+            : "z-10 absolute left-0 top-0 h-[100%] bg-black w-[60%] text-white  ease-in-out  duration-500"
+        }
+      >
+        <h1 className="h-[13vh] w-[40%] flex  md:hidden ">
+          <Link to="/">
+            <img
+              className="w-[100%] m-2 justify-center items-center "
+              src={logo}
+              alt="SIXpo logo: a bright pink heart with the word SIXPO in yellow across the front and the word Festival printed in outlined letters below."
+            />
+          </Link>
+        </h1>
+        <ul className="flex flex-col items-start uppercase w-[100%] pt-2  md:hidden ">
+          <li className="my-1 p-2 w-[100%]  hover:ease-in-out hover:duration-500 hover:underline hover:text-[#ffef91]">
+            <Link to="/home">Home</Link>
+          </li>
+          <li className="my-1 p-2 w-[100%]  hover:ease-in-out hover:duration-500 hover:underline hover:text-[#ffef91]">
+            <Link to="/about">About Us</Link>
+          </li>
+          <li className="my-1 p-2 w-[100%]  hover:ease-in-out hover:duration-500 hover:underline hover:text-[#ffef91]">
+            <Link to="/events">Events</Link>
+          </li>
+          <li className="my-1 p-2 w-[100%]  hover:ease-in-out hover:duration-500 hover:underline hover:text-[#ffef91]">
+            <Link to="/blog">Our Blog</Link>
+          </li>
+          <li className="my-1 p-2 w-[100%]  hover:ease-in-out hover:duration-500 hover:underline hover:text-[#ffef91]">
+            <Link to="/resources">Resources</Link>
+          </li>
+        </ul>
+        <ul className="flex mx-2 text-white text-2xl pt-2">
+          <a href="https://www.instagram.com/yvrsixpo/" target="_blank">
+            {" "}
+            <li className="mr-2">
+              <AiFillInstagram />
+            </li>
+          </a>
+          <a href="https://www.facebook.com/yvrsixpo/" target="_blank">
+            {" "}
+            <li>
+              <AiFillFacebook />
+            </li>
+          </a>
+        </ul>
+      </div>
     </nav>
-    
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
