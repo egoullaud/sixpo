e52e969fc069a313d634369b3448a01a852c93a7
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-22 18:53:43 -0700

    formatted with prettier

--- src/components/blog/SearchBar.jsx
+++ src/components/blog/SearchBar.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import BlogSearchResults from './BlogSearchResults'
+import React, { useEffect, useState } from "react";
+import BlogSearchResults from "./BlogSearchResults";
 
-
-const BLOG_CATEGORY_SEARCH_QUERY =`
+const BLOG_CATEGORY_SEARCH_QUERY = `
 {
 blogCategories {
     id
@@ -23,57 +22,61 @@ blogCategories {
     }
   }
 }
-`
-
+`;
 
 function SearchBar() {
-    const [searchTerm, setSearchTerm] = useState("")
-    const [searchResults, setSearchResults] = useState([])
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
-    const handleChange = (e) => {
-        setSearchTerm(e.target.value)
-    }
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
-    const onSubmit = (e) => {
-        e.preventDefault()
-        // let slug = searchTerm.split('').join('-').toLowerCase()
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // let slug = searchTerm.split('').join('-').toLowerCase()
 
-        setSearchResults([])
-        fetch('https://ca-central-1.cdn.hygraph.com/content/cleeq0yjw61mv01uo3jtbetgq/master', {
-            method: "POST",
-            headers: {"Content-Type" : "application/json"},
-            body: JSON.stringify({query: BLOG_CATEGORY_SEARCH_QUERY})
-        }).then(response=> response.json())
-        .then(data =>console.log(data.data.searchResults))
-        setSearchTerm("")
+    setSearchResults([]);
+    fetch(
+      "https://ca-central-1.cdn.hygraph.com/content/cleeq0yjw61mv01uo3jtbetgq/master",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ query: BLOG_CATEGORY_SEARCH_QUERY }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data.data.searchResults));
+    setSearchTerm("");
 
+    // fetch('https://ca-central-1.cdn.hygraph.com/content/cleeq0yjw61mv01uo3jtbetgq/master')
+  };
 
-        // fetch('https://ca-central-1.cdn.hygraph.com/content/cleeq0yjw61mv01uo3jtbetgq/master')
-    }
-    
-// useEffect(() => {
-//     fetch('https://ca-central-1.cdn.hygraph.com/content/cleeq0yjw61mv01uo3jtbetgq/master', {
-//         method: "POST",
-//         headers: {"Content-Type" : "application/json"},
-//         body: JSON.stringify({query: BLOG_CATEGORY_SEARCH_QUERY})
-//     }).then(response=> response.json())
-//     .then(data =>console.log(data.data.searchResults))
-//     // .then(data => setSearchResults(data.data.searchResults))
-// }, []);
-   
+  // useEffect(() => {
+  //     fetch('https://ca-central-1.cdn.hygraph.com/content/cleeq0yjw61mv01uo3jtbetgq/master', {
+  //         method: "POST",
+  //         headers: {"Content-Type" : "application/json"},
+  //         body: JSON.stringify({query: BLOG_CATEGORY_SEARCH_QUERY})
+  //     }).then(response=> response.json())
+  //     .then(data =>console.log(data.data.searchResults))
+  //     // .then(data => setSearchResults(data.data.searchResults))
+  // }, []);
+
   return (
     <div>
       <form onSubmit={onSubmit}>
-          <input type="text" 
-          placeholder='search blog'
+        <input
+          type="text"
+          placeholder="search blog"
           value={searchTerm}
-          onChange={handleChange} 
-          className='border-[2px] border-gray-500 rounded-lg text-center p-2 text-lg active:border-[#ff5b5b]'/>
-          <input type="submit" className='hidden'/>
-        </form>
-        <BlogSearchResults searchResults={searchResults}/>
+          onChange={handleChange}
+          className="border-[2px] border-gray-500 rounded-lg text-center p-2 text-lg active:border-[#ff5b5b]"
+        />
+        <input type="submit" className="hidden" />
+      </form>
+      <BlogSearchResults searchResults={searchResults} />
     </div>
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
--- src/components/blog/SelectedBlogCatCard.jsx
+++ src/components/blog/SelectedBlogCatCard.jsx
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
-import {request} from 'graphql-request'
-import moment from 'moment'
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { request } from "graphql-request";
+import moment from "moment";
+import { Link } from "react-router-dom";
 
 function SelectedBlogCatCard() {
-    const [blogCategory, setBlogCategory] = useState([]);
-    const { slug } = useParams();
+  const [blogCategory, setBlogCategory] = useState([]);
+  const { slug } = useParams();
 
-    useEffect(() => {
-        const fetchBlogCategory = async () => {
-        const { blogCategory } = await  
-        request('https://ca-central-1.cdn.hygraph.com/content/cleeq0yjw61mv01uo3jtbetgq/master', `
+  useEffect(() => {
+    const fetchBlogCategory = async () => {
+      const { blogCategory } = await request(
+        "https://ca-central-1.cdn.hygraph.com/content/cleeq0yjw61mv01uo3jtbetgq/master",
+        `
         {
             blogCategory(where: {slug: "${slug}"}) {
               posts {
@@ -30,53 +31,66 @@ function SelectedBlogCatCard() {
             }
           }
         `
-        );
-        console.log(blogCategory);
-        setBlogCategory(blogCategory);
+      );
+      console.log(blogCategory);
+      setBlogCategory(blogCategory);
     };
     fetchBlogCategory();
-}, [slug]);
+  }, [slug]);
 
-    
   return (
-   
-     <div className='flex items-center justify-center'>
-            {blogCategory.posts?.map((post) =>(
-        <card className='bg-white rounded shadow-md flex flex-col items-center justify-center
+    <div className="flex items-center justify-center">
+      {blogCategory.posts?.map((post) => (
+        <card
+          className="bg-white rounded shadow-md flex flex-col items-center justify-center
         w-[90%] 
         md:w-[90%] 
-        lg:flex-row lg:w-[80%]'
-          key={post.id}>
-       <Link className='w-[100%] object-cover lg:ml-4
+        lg:flex-row lg:w-[80%]"
+          key={post.id}
+        >
+          <Link
+            className="w-[100%] object-cover lg:ml-4
                         lg:w-[50%]
-       xl:m-4' to={"/blog/"+post.slug}> 
-          <img  className='rounded' src={post.image.url} alt={post.image.altText} />
-       </Link>
-        <div className='flex flex-col text-center mr-4 mb-4
-                      md:text-left'>
-          <h3 className=' w-[90%]  font-bold mx-4 mt-2 
+       xl:m-4"
+            to={"/blog/" + post.slug}
+          >
+            <img
+              className="rounded"
+              src={post.image.url}
+              alt={post.image.altText}
+            />
+          </Link>
+          <div
+            className="flex flex-col text-center mr-4 mb-4
+                      md:text-left"
+          >
+            <h3
+              className=" w-[90%]  font-bold mx-4 mt-2 
                     lg:mt-6 md:text-xl
-                    lg:text-2xl' >{post.title}</h3>
-        <h4 className='font-semibold mt-3 mx-4' >
-            {/* map over authors array */}
-            {post.authors?.map((author) => 
-        {return (`${author.name}`)})}  | {moment(post.datePublished).format('MMM DD, YYYY')} </h4>
-        <p className='text-base mx-4 my-2'  >{post.preview} <Link className='font-semibold mx-1' to={"/blog/"+post.slug}><button className='underline hover:font-bold transition-all duration-700'>
-            Read More</button></Link></p>
-        </div>
-        
-
-</card>
-
-          
-           
-          ))} 
-
-        
-
-        </div>
-    
-  )
+                    lg:text-2xl"
+            >
+              {post.title}
+            </h3>
+            <h4 className="font-semibold mt-3 mx-4">
+              {/* map over authors array */}
+              {post.authors?.map((author) => {
+                return `${author.name}`;
+              })}{" "}
+              | {moment(post.datePublished).format("MMM DD, YYYY")}{" "}
+            </h4>
+            <p className="text-base mx-4 my-2">
+              {post.preview}{" "}
+              <Link className="font-semibold mx-1" to={"/blog/" + post.slug}>
+                <button className="underline hover:font-bold transition-all duration-700">
+                  Read More
+                </button>
+              </Link>
+            </p>
+          </div>
+        </card>
+      ))}
+    </div>
+  );
 }
 
-export default SelectedBlogCatCard
+export default SelectedBlogCatCard;
